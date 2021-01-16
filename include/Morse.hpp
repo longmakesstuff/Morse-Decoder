79f103b9338240a679f10980b56eea4414312cec
Long Nguyen <long.nguyen2306@protonmail.com>
2021-01-16 18:36:16 +0100

    Yay?

--- include/Morse.hpp
+++ include/Morse.hpp
@@ -4,62 +4,33 @@
 #include <string>
 #include <map>
 #include <optional>
+#include <sstream>
+#include "String.hpp"
 
-typedef enum TokenType {
-    DOT, DASH
-} TokenType;
+constexpr char DOT = '.';
+constexpr char DASH = '-';
+constexpr char SPACE = ' ';
 
-class Token {
-    uint64_t t_start;
-    uint64_t t_end;
-    uint64_t t_duration{};
-    TokenType tokenType{};
-public:
-    Token(TokenType _tokenType);
-
-    Token(Token && token) noexcept;
-
-    Token(const Token & token);
-
-    Token(uint64_t tStart, uint64_t tEnd);
-
-    bool operator==(const Token &rhs) const;
-
-    bool operator!=(const Token &rhs) const;
-
-    bool operator<(const Token &rhs) const;
-
-    bool operator>(const Token &rhs) const;
-
-    bool operator<=(const Token &rhs) const;
-
-    bool operator>=(const Token &rhs) const;
-};
-
-class Sequence {
+class Morse {
 private:
-    std::vector<Token> tokens;
-    static std::map<Sequence, std::string> map;
+    std::map<std::string, std::string> map{};
+    std::vector<std::string> tokens;
 public:
-    explicit Sequence(const std::vector<Token>& tokens);
-
-    Sequence(const std::vector<TokenType>& tokenTypes);
-
-    std::optional<std::string> addToken(const Token& token);
+    Morse();
 
     std::optional<std::string> parse();
 
-    bool operator<(const Sequence &rhs) const;
+    bool operator<(const Morse &rhs) const;
 
-    bool operator>(const Sequence &rhs) const;
+    bool operator>(const Morse &rhs) const;
 
-    bool operator<=(const Sequence &rhs) const;
+    bool operator<=(const Morse &rhs) const;
 
-    bool operator>=(const Sequence &rhs) const;
+    bool operator>=(const Morse &rhs) const;
 
-    bool operator==(const Sequence &rhs) const;
+    bool operator==(const Morse &rhs) const;
 
-    bool operator!=(const Sequence &rhs) const;
+    bool operator!=(const Morse &rhs) const;
 };
 
-typedef std::pair< Sequence, std::string> MorsePair;
+typedef std::pair<std::string, std::string> MorsePair;
